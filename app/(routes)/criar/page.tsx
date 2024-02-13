c44c9e49cb7bbea8b4609a29b09ff2d175f25581
PSjoon <pedrosantos.joon@gmail.com>
2024-02-13 16:17:35 -0300

    start post routes

---
+++ app/(routes)/criar/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Página de Criação",
+  description: "create post page",
+}
+
+export default function Criar() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
---
+++ app/(routes)/visualizar/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Página de Visualização",
+  description: "view post page",
+}
+
+export default function Visualizar() {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  )
+}
