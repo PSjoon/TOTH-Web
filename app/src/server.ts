f0db1881bcffe2dce3131219f5382a43d31da7da
PSjoon <pedrosantos.joon@gmail.com>
2024-02-16 20:30:04 -0300

    restart commits

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,30 +1,26 @@
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 import express from 'express'
-// import bodyParser from 'body-parser'
-// import { createArticle } from './createArticle'
-// import { viewArticle } from './viewArticle'
-// import cors from 'cors'
+import bodyParser from 'body-parser'
+import { createArticle } from './createArticle'
+import { viewArticle } from './viewArticle'
+import cors from 'cors'
 
 const app = express()
-// dotenv.config({ path: '.env' })
-// dotenv.config({})
-// app.use(cors())
+dotenv.config({ path: '.env' })
+dotenv.config({})
+app.use(cors())
 
-// const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3334
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3334
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-// createArticle(app)
-// viewArticle(app)
-
-app.get('/', () => {
-  console.log('Hello world')
-})
+createArticle(app)
+viewArticle(app)
 
 try {
-  app.listen(3333, () => {
-    console.log(`Servidor iniciado na porta 3333`)
+  app.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`)
   })
 } catch (error) {
   console.log(error)
