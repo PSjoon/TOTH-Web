a51d877f2c6c6ac843ad08a076d7e3a224bd4cbe
PSjoon <pedrosantos.joon@gmail.com>
2024-03-17 14:52:23 -0300

    remove

--- app/src/api/auth/callback/route.ts
+++ app/src/api/auth/callback/route.ts
@@ -2,31 +2,27 @@ import { NextRequest, NextResponse } from 'next/server'
 import { api } from '../../../lib/api'
 
 export async function GET(req: NextRequest) {
-  try {
-    const { searchParams } = new URL(req.url)
-    const code = searchParams.get('code')
+  const { searchParams } = new URL(req.url)
+  const code = searchParams.get('code')
 
-    console.log(code)
-    console.log(1)
+  console.log(code)
+  console.log(1)
 
-    const redirectTo = req.cookies.get('redirectTo')?.value
+  const redirectTo = req.cookies.get('redirectTo')?.value
 
-    const registerResponse = await api.post('/githubauth', {
-      code,
-    })
+  const registerResponse = await api.post('/githubauth', {
+    code,
+  })
 
-    const { token } = registerResponse.data
+  const { token } = registerResponse.data
 
-    const redirectURL = redirectTo ?? new URL('/', req.url)
+  const redirectURL = redirectTo ?? new URL('/', req.url)
 
-    const cookieExpiresSec = 60 * 60 * 24 * 30
+  const cookieExpiresSec = 60 * 60 * 24 * 30
 
-    return NextResponse.redirect(redirectURL, {
-      headers: {
-        'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresSec}`,
-      },
-    })
-  } catch (error) {
-    console.log(error)
-  }
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresSec}`,
+    },
+  })
 }
